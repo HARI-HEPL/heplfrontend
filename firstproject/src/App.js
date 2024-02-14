fb1ff33a7ee44c6f45e543320c2777716ded0c48
Nalini2203 <159430830+Nalini2203@users.noreply.github.com>
2024-02-14 16:22:17 +0530

    Changes in details

--- firstproject/src/App.js
+++ firstproject/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello Everyone, Welcome to React</h1>
+        <h2>Welcome All</h2>
         
         <p>
           Edit <code>src/App.js</code> and save to reload.
