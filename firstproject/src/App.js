2c7309bd095e0f2187a5637f6f6e439aacedb803
Vignesh-HEPL <159533640+Vignesh-HEPL@users.noreply.github.com>
2024-02-16 11:19:04 +0530

    File Handling

--- firstproject/src/App.js
+++ firstproject/src/App.js
@@ -1,27 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+
+import React, { useState } from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+// button
 
 function App() {
+  const [files, setFiles] = useState([]);
+
+  const handleFileChange = (e) => {
+    const newFiles = Array.from(e.target.files);
+    setFiles([...files, ...newFiles]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>srinath</h1>
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2> <br></br> <center>File Handling</center><br></br></h2>
+      <input type="file" onChange={handleFileChange} multiple />
+      
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>File Type</TableCell>
+              <TableCell>View</TableCell>
+              {/* <TableCell>Download</TableCell>  */}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {files.map((file, index) => (
+              <TableRow key={index}>
+                <TableCell>{file.type}</TableCell>
+                <TableCell>
+                  {file.type.startsWith('image/') ? (
+                    <img src={URL.createObjectURL(file)} alt="Preview" width="100" height="100" />
+                  ) : file.type.startsWith('video/') ? (
+                    <video controls width="200">
+                      <source src={URL.createObjectURL(file)} type={file.type} />
+                      Your browser does not support the video tag.
+                    </video>
+                  ) : (
+                    <div>{file.name}</div>
+                  )}
+                </TableCell>
+                {/* <TableCell>
+                  <Button component="a" href={URL.createObjectURL(file)} download>
+                    Download
+                  </Button>
+                </TableCell> */}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
 
 export default App;
+
